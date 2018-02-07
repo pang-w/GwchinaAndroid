a1dbfa72ea8229891e4ff0478f2929161398efae
zhangjg_lw <zhangjg@txtws.com>
2018-02-07 18:45:58 +0800

    update 20180207

--- run_all/run_all_test.py
+++ run_all/run_all_test.py
@@ -19,18 +19,6 @@ report = absPath + "\\report\\report_html\\"
 page_path = absPath + '\\data\\page\\'
 loc_now_path = absPath + '\\data\\loc_now\\'
 
-node = phoneGetConfigInfo.ReadConfig().readAppiumData()['config']
-
-try:
-    sleep(5)
-    '''stopAppiumServer'''
-    AppiumServer.appiumServerNew().stopServer()
-finally:
-    '''startAppiumServer'''
-    sleep(5)
-    AppiumServer.appiumServerNew().startServer(node)
-    sleep(20)
-
 log.info('删除page_path图片,删除loc_now_path图片')
 DefNowPng.del_files(page_path)
 DefNowPng.del_files(loc_now_path)
@@ -41,37 +29,55 @@ def Creatsuite():
     for test_suite in discover:
         for caseName in test_suite:
             testUnit.addTest(caseName)
-        print(testUnit)
+        # print(testUnit)
     return testUnit
 
-test_case = Creatsuite()
-now = time.strftime('%Y-%m-%d-%H_%M_%S', time.localtime(time.time()))
-day = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+def reportHtml():
+    test_case = Creatsuite()
+    now = time.strftime('%Y-%m-%d-%H_%M_%S', time.localtime(time.time()))
+    day = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
-tdResult = report + day
-if os.path.exists(tdResult):
-    filename = tdResult + "\\" + now + "_result.html"
-    fp = open(filename,'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(stream = fp, title ='格雷盒子Android家长端测试报告', description ='用例执行情况： ')
-    runner.run(test_case)
-    print('-------------------------- 测试结束 --------------------------')
-    fp.close()
-else:
-    os.mkdir(tdResult)
-    filename = tdResult + "\\" + now + "_result.html"
-    fp = open(filename,'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(stream = fp, title ='格雷盒子Android家长端测试报告', description ='用例执行情况： ')
-    runner.run(test_case)
-    print('-------------------------- 测试结束 --------------------------')
-    fp.close()
+    tdResult = report + day
+    if os.path.exists(tdResult):
+        filename = tdResult + "\\" + now + "_result.html"
+        fp = open(filename,'wb')
+        runner = HTMLTestRunner.HTMLTestRunner(stream = fp, title ='格雷盒子Android家长端测试报告', description ='用例执行情况： ')
+        runner.run(test_case)
+        print('-------------------------- 测试结束 --------------------------')
+        fp.close()
+    else:
+        os.mkdir(tdResult)
+        filename = tdResult + "\\" + now + "_result.html"
+        fp = open(filename,'wb')
+        runner = HTMLTestRunner.HTMLTestRunner(stream = fp, title ='格雷盒子Android家长端测试报告', description ='用例执行情况： ')
+        runner.run(test_case)
+        print('-------------------------- 测试结束 --------------------------')
+        fp.close()
 
-# html_path = os.path.dirname(os.path.abspath('.')) + '\\report\\report_html\\' + day
-# log_path = os.path.dirname(os.path.abspath('.')) + '\\log\\' + day
-#
-# HtmlPath = SendEmail.FindNewHtml(html_path)
-# LogPath = SendEmail.FindNewLog(log_path)
-# SendEmail.SendEmail(HtmlPath,LogPath)
+def sendEmail():
+    html_path = os.path.dirname(os.path.abspath('.')) + '\\report\\report_html\\' + day
+    log_path = os.path.dirname(os.path.abspath('.')) + '\\log\\' + day
 
-sleep(5)
-'''stopAppiumServer'''
-AppiumServer.appiumServerNew().stopServer()
+    HtmlPath = SendEmail.FindNewHtml(html_path)
+    LogPath = SendEmail.FindNewLog(log_path)
+    SendEmail.SendEmail(HtmlPath,LogPath)
+
+if __name__ == '__main__':
+    node = phoneGetConfigInfo.ReadConfig().readAppiumData()['config']
+    try:
+        sleep(5)
+        '''stopAppiumServer'''
+        AppiumServer.appiumServerNew().stopServer()
+    finally:
+        '''startAppiumServer'''
+        sleep(5)
+        AppiumServer.appiumServerNew().startServer(node)
+        sleep(20)
+
+    Creatsuite()
+    reportHtml()
+    sendEmail()
+
+    sleep(5)
+    '''stopAppiumServer'''
+    AppiumServer.appiumServerNew().stopServer()
